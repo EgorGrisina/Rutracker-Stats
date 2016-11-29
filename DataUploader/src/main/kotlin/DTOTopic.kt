c226b9722b1d0abe371027b85a94551d303d21cd
Egor Gritsina _home <egorgrisina@gmail.com>
2016-11-30 02:55:47 +0300

    topics base

---
+++ DataUploader/src/main/kotlin/DTOTopic.kt
@@ -0,0 +1,13 @@
+
+/**
+ * Created by egorg on 30.11.2016.
+ */
+
+data class DTOTopic(
+        val topic_title : String?,
+        val forum_name : String?,
+        val forum_id : Int?,
+        val size : Long?,
+        val seeders : Int?,
+        val live : Boolean?
+)
--- DataUploader/src/main/kotlin/Main.kt
+++ DataUploader/src/main/kotlin/Main.kt
@@ -1,32 +1,51 @@
-import kotlinx.nosql.*
-import kotlinx.nosql.mongodb.DocumentSchema
-import kotlinx.nosql.mongodb.MongoDB
+import org.litote.kmongo.*
+import org.litote.kmongo.util.KMongoUtil.toBson
+import java.util.*
 
-/**
- * Created by EGritsina on 28.11.2016.
- */
+val HOST = "egritsina.domain.corp"
+val PORT = 27017
+val DB_NAME = "Rutracker"
+val COLLECTION_TOPICS = "topics"
 
-object Users : DocumentSchema<User>("users", User::class) {
-    val id = integer("id")
-    val name = string("name")
-}
-
-object Days : DocumentSchema<Day>("days", Day::class) {
-    val name = string("name")
-    val weekend = boolean("weekend")
-    val users = integer("users")
-}
-
-data class Day(val name: String, val weekend: Boolean, val users : Int)
-data class User(val id : Int, val name: String)
+//data class Day(val _id: String?, val name: String, val weekend: Boolean, var users : Long)
 
 fun main(args: Array<String>) {
     println("Hello kotlin!")
-    val db = MongoDB(database = "test", schemas = arrayOf(Days))
+    /*val mongoURI = MongoClientURI("mongodb://egritsina.domain.corp:27017/test")
+    val db = MongoDB(mongoURI, schemas = arrayOf(Days))
 
     db.withSession {
         var result = Days.find{name.equal("Monday")}.projection { users }.update(1500)
         println(result)
+    }*/
+    val client = KMongo.createClient(HOST, PORT)
+    val database = client.getDatabase(DB_NAME) //normal java driver usage
+    val collection = database.getCollection<DTOTopic>(COLLECTION_TOPICS) //KMongo extension method
+
+    val topics = ArrayList<DTOTopic>()
+    val random = Random()
+
+    for (i in 0..200) {
+        val forum_id = random.nextInt(10)
+        val seeds = random.nextInt(100)
+        val topic = DTOTopic("Фильм $i", "Форум $forum_id", forum_id, size = random.nextInt(10000000).toLong(), seeders = seeds,  live = seeds>20 )
+        topics.add(topic)
     }
+
+
+    collection.insertMany(topics)
+
+
+
+    println("Result: ")
+    val result = collection.find()
+    for (topic in result) {
+        println(topic)
+    }
+
+
+    /*val newday = result.toList().get(0)
+    newday.users = 3000
+    println(collection.updateOne("{name: '${newday.name}'}", "{\$set: {users : ${newday.users}} }"))*/
 
 }
